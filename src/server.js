4871445f7645c32d5ea0f62a045bad7cc4383204
Daniel <114115064+Canu80@users.noreply.github.com>
2023-09-07 17:53:54 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -39,29 +39,9 @@ hbs.handlebars.registerHelper('ifCond', function(v1, operator, v2, options) {
   switch (operator) {
     case '==':
       return (v1 == v2) ? options.fn(this) : options.inverse(this);
-    case '===':
-      return (v1 === v2) ? options.fn(this) : options.inverse(this);
-    case '!=':
-      return (v1 != v2) ? options.fn(this) : options.inverse(this);
-    case '!==':
-      return (v1 !== v2) ? options.fn(this) : options.inverse(this);
-    case '<':
-      return (v1 < v2) ? options.fn(this) : options.inverse(this);
-    case '<=':
-      return (v1 <= v2) ? options.fn(this) : options.inverse(this);
-    case '>':
-      return (v1 > v2) ? options.fn(this) : options.inverse(this);
-    case '>=':
-      return (v1 >= v2) ? options.fn(this) : options.inverse(this);
     default:
       return options.inverse(this);
   }
-});
-hbs.handlebars.registerHelper('multiply', function(a, b) {
-  return a * b;
-});
-hbs.handlebars.registerHelper('addition', function(a, b) {
-  return a + b;
 });
 
 // Middlewares
